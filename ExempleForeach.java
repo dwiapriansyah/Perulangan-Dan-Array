7ec199401b14802ed222cf32b210a1310509e336
Dwi Apriansyah <115854672+dwiapriansyah@users.noreply.github.com>
2022-12-17 16:48:28 +0700

    Add files via upload

---
+++ ExempleForeach.java
@@ -0,0 +1,9 @@
+public class ExempleForeach {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars = {"red", "green", "blue", "yellow"};
+		for (String i : cars) {
+			System.out.println(i);
+		}
+	}
+}
